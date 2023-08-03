2a56fd33fe94157c126342e49065ab6438894f64
Sezmek <97065268+Sezmek@users.noreply.github.com>
2023-08-03 15:03:12 +0200

    Add Spawning and seting pieces on tilemap

--- Assets/Sripts/Board.cs
+++ Assets/Sripts/Board.cs
@@ -1,8 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominos;
+    public Vector3Int SpawnPositon;
+
+    private void Awake()
+    {
+        tilemap = GetComponentInChildren<Tilemap>();
+        activePiece = GetComponentInChildren<Piece>();
+        for (int i = 0; i < tetrominos.Length; i++)
+        {
+            tetrominos[i].Initialize();
+        }
+    }
+    private void Start()
+    {
+        SpawnPiece();
+    }
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, tetrominos.Length);
+        TetrominoData data = tetrominos[random];
+
+        activePiece.Initialize(this, SpawnPositon, data);
+        Set(activePiece);
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i<piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
 }
